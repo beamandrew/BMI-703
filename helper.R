6f0717f5b92e913dd2e53d6e1b9b0eecbf004d40
beamandrew <beam.andrew@gmail.com>
2017-10-07 15:24:45 -0400

    helper functions

---
+++ helper.R
@@ -0,0 +1,19 @@
+sigmoid <- function(outputs) {
+  return(plogis(outputs))
+} 
+
+dW_dL <- function(labels, inputs,outputs) {
+  return( t(labels- outputs)%*%inputs )
+}
+
+log_loss <- function(y_true,y_pred) {
+  y_pred <- pmax(y_pred , 1e-8)
+  y_pred <- pmin(y_pred, 0.999999)
+  ll = -mean(y_true*log(y_pred) + (1-y_true)*log(1-y_pred))
+  return(ll)
+}
+
+binary_accuracy <- function(y_true,y_pred) {
+  y_pred <- round(y_pred)
+  return(length(which(y_true == y_pred))/length(y_true))
+}
